45553f3f068f1b7ba33435ddf91b98e3da0fd94d
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2019-12-04 14:20:14 +0100

    feat: first version of template

--- bookmarks_collection/main.py
+++ bookmarks_collection/main.py
@@ -1,13 +1,36 @@
 import json
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pathlib import Path
+
 from objects import BookmarkRoot
+
+current_dir = Path(__file__).parent
+template_dir = str(current_dir / "templates")
+
+env = Environment(
+    loader=FileSystemLoader(template_dir), autoescape=select_autoescape(["html"])
+)
+
+index_template = env.get_template("index.html")
 
 
 def main():
     with open("bookmarks-2019-07-20-simple.json") as f:
         bookmarks_json = json.load(f)
         bookmarks = BookmarkRoot(**bookmarks_json)
-        print(bookmarks)
+        toolbar_root: BookmarkRoot = next(
+            bookmark
+            for bookmark in bookmarks.children
+            if bookmark.guid == "toolbar_____"
+        )
+        informatique_root: BookmarkRoot = next(
+            bookmark
+            for bookmark in toolbar_root.children
+            if bookmark.guid == "4R2TUlyxQpbx"
+        )
+    with open("index.html", "w") as f:
+        f.write(index_template.render(bookmarks_root=informatique_root))
 
 
 if __name__ == "__main__":
